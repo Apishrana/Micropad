cf2ac010446004afb0bb70ca0070eb003cb8f464
Apishrana <apishrana682@gmail.com>
2026-09-16 16:07:35 +0530

    added lopaka genrated screen code

---
+++ src/display.py
@@ -0,0 +1,164 @@
+import displayio
+import terminalio
+from adafruit_display_text import label
+from adafruit_display_shapes.rect import Rect
+
+
+# [BEGIN lopaka generated]
+image_imgbitmap_png_bits = displayio.OnDiskBitmap("/image_imgbitmap_png_bits.bmp")
+image_Volup_bits = displayio.OnDiskBitmap("/image_Volup_bits.bmp")
+
+# Init display
+Default = displayio.Group()
+display.root_group = Default
+
+def drawDefault():
+    # rect 1
+    rect_1 = Rect(4, 10, 7, 51, outline=0xFFFFFF)
+    Default.append(rect_1)
+    # rect 1
+    rect_1 = Rect(117, 10, 7, 51, outline=0xFFFFFF)
+    Default.append(rect_1)
+    # Volup
+    image_Volup_tile = displayio.TileGrid(image_Volup_bits, pixel_shader=image_Volup_bits.pixel_shader, position=(3, 2))
+    Default.append(image_Volup_tile)
+    # imgbitmap.png
+    image_imgbitmap_png_tile = displayio.TileGrid(image_imgbitmap_png_bits, pixel_shader=image_imgbitmap_png_bits.pixel_shader, position=(117, 2))
+    Default.append(image_imgbitmap_png_tile)
+    # string 5
+    string_5 = label.Label(terminalio.FONT, text="88:88", color=0xFFFFFF)
+    string_5.x = 21
+    string_5.y = 32
+    string_5.scale = 2
+    Default.append(string_5)
+    # string 6
+    string_6 = label.Label(terminalio.FONT, text="AM", color=0xFFFFFF)
+    string_6.x = 83
+    string_6.y = 32
+    string_6.scale = 2
+    Default.append(string_6)
+    # rect 7
+    rect_7 = Rect(4, 48, 7, 12, fill=0xFFFFFF, outline=0xFFFFFF)
+    Default.append(rect_7)
+    # rect 7
+    rect_7 = Rect(117, 48, 7, 12, fill=0xFFFFFF, outline=0xFFFFFF)
+    Default.append(rect_7)
+# [END lopaka generated]
+
+
+
+# [BEGIN lopaka generated]
+image_imgbitmap_png_bits = displayio.OnDiskBitmap("/image_imgbitmap_png_bits.bmp")
+image_music_play_bits = displayio.OnDiskBitmap("/image_music_play_bits.bmp")
+image_Volup_bits = displayio.OnDiskBitmap("/image_Volup_bits.bmp")
+
+# Init display
+media__play_icon_ = displayio.Group()
+display.root_group = media__play_icon_
+
+def drawmedia__play_icon_():
+    # rect 1
+    rect_1 = Rect(4, 10, 7, 51, outline=0xFFFFFF)
+    media__play_icon_.append(rect_1)
+    # rect 1
+    rect_1 = Rect(117, 10, 7, 51, outline=0xFFFFFF)
+    media__play_icon_.append(rect_1)
+    # Volup
+    image_Volup_tile = displayio.TileGrid(image_Volup_bits, pixel_shader=image_Volup_bits.pixel_shader, position=(3, 2))
+    media__play_icon_.append(image_Volup_tile)
+    # imgbitmap.png
+    image_imgbitmap_png_tile = displayio.TileGrid(image_imgbitmap_png_bits, pixel_shader=image_imgbitmap_png_bits.pixel_shader, position=(117, 2))
+    media__play_icon_.append(image_imgbitmap_png_tile)
+    # string 5
+    string_5 = label.Label(terminalio.FONT, text="88:88", color=0xFFFFFF)
+    string_5.x = 42
+    string_5.y = 11
+    media__play_icon_.append(string_5)
+    # string 6
+    string_6 = label.Label(terminalio.FONT, text="AM", color=0xFFFFFF)
+    string_6.x = 74
+    string_6.y = 11
+    media__play_icon_.append(string_6)
+    # rect 7
+    rect_7 = Rect(4, 48, 7, 12, fill=0xFFFFFF, outline=0xFFFFFF)
+    media__play_icon_.append(rect_7)
+    # rect 7
+    rect_7 = Rect(117, 48, 7, 12, fill=0xFFFFFF, outline=0xFFFFFF)
+    media__play_icon_.append(rect_7)
+    # string 17
+    string_17 = label.Label(terminalio.FONT, text="Current", color=0xFFFFFF)
+    string_17.x = 42
+    string_17.y = 31
+    media__play_icon_.append(string_17)
+    # rect 18
+    rect_18 = Rect(21, 54, 86, 5, outline=0xFFFFFF)
+    media__play_icon_.append(rect_18)
+    # rect 19
+    rect_19 = Rect(21, 55, 21, 4, fill=0xFFFFFF, outline=0xFFFFFF)
+    media__play_icon_.append(rect_19)
+    # circle 20
+    circle_20 = Circle(41, 56, 4, fill=0xFFFFFF, outline=0xFFFFFF)
+    media__play_icon_.append(circle_20)
+    # music_play
+    image_music_play_tile = displayio.TileGrid(image_music_play_bits, pixel_shader=image_music_play_bits.pixel_shader, position=(61, 42))
+    media__play_icon_.append(image_music_play_tile)
+# [END lopaka generated]
+
+
+
+# [BEGIN lopaka generated]
+image_imgbitmap_png_bits = displayio.OnDiskBitmap("/image_imgbitmap_png_bits.bmp")
+image_music_play_bits = displayio.OnDiskBitmap("/image_music_play_bits.bmp")
+image_Volup_bits = displayio.OnDiskBitmap("/image_Volup_bits.bmp")
+
+# Init display
+media__pause_icon_ = displayio.Group()
+display.root_group = media__pause_icon_
+
+def drawmedia__pause_icon_():
+    # rect 1
+    rect_1 = Rect(4, 10, 7, 51, outline=0xFFFFFF)
+    media__pause_icon_.append(rect_1)
+    # rect 1
+    rect_1 = Rect(117, 10, 7, 51, outline=0xFFFFFF)
+    media__pause_icon_.append(rect_1)
+    # Volup
+    image_Volup_tile = displayio.TileGrid(image_Volup_bits, pixel_shader=image_Volup_bits.pixel_shader, position=(3, 2))
+    media__pause_icon_.append(image_Volup_tile)
+    # imgbitmap.png
+    image_imgbitmap_png_tile = displayio.TileGrid(image_imgbitmap_png_bits, pixel_shader=image_imgbitmap_png_bits.pixel_shader, position=(117, 2))
+    media__pause_icon_.append(image_imgbitmap_png_tile)
+    # string 5
+    string_5 = label.Label(terminalio.FONT, text="88:88", color=0xFFFFFF)
+    string_5.x = 42
+    string_5.y = 11
+    media__pause_icon_.append(string_5)
+    # string 6
+    string_6 = label.Label(terminalio.FONT, text="AM", color=0xFFFFFF)
+    string_6.x = 74
+    string_6.y = 11
+    media__pause_icon_.append(string_6)
+    # rect 7
+    rect_7 = Rect(4, 48, 7, 12, fill=0xFFFFFF, outline=0xFFFFFF)
+    media__pause_icon_.append(rect_7)
+    # rect 7
+    rect_7 = Rect(117, 48, 7, 12, fill=0xFFFFFF, outline=0xFFFFFF)
+    media__pause_icon_.append(rect_7)
+    # string 17
+    string_17 = label.Label(terminalio.FONT, text="Current", color=0xFFFFFF)
+    string_17.x = 42
+    string_17.y = 31
+    media__pause_icon_.append(string_17)
+    # rect 18
+    rect_18 = Rect(21, 54, 86, 5, outline=0xFFFFFF)
+    media__pause_icon_.append(rect_18)
+    # rect 19
+    rect_19 = Rect(21, 55, 21, 4, fill=0xFFFFFF, outline=0xFFFFFF)
+    media__pause_icon_.append(rect_19)
+    # circle 20
+    circle_20 = Circle(41, 56, 4, fill=0xFFFFFF, outline=0xFFFFFF)
+    media__pause_icon_.append(circle_20)
+    # music_play
+    image_music_play_tile = displayio.TileGrid(image_music_play_bits, pixel_shader=image_music_play_bits.pixel_shader, position=(61, 42))
+    media__pause_icon_.append(image_music_play_tile)
+# [END lopaka generated]
