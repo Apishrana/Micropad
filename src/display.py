90a7c0400ac1aeb005a061b965dd89c50b4cb387
Apishrana <apishrana682@gmail.com>
2026-09-16 16:31:52 +0530

    fixed the autodenrated variable names

--- src/display.py
+++ src/display.py
@@ -4,10 +4,10 @@ from adafruit_display_text import label
 from adafruit_display_shapes.rect import Rect
 from adafruit_display_shapes.circle import Circle
 
-image_imgbitmap_png_bits = displayio.OnDiskBitmap("/image_imgbitmap_png_bits.bmp")
-image_Volup_bits = displayio.OnDiskBitmap("/image_Volup_bits.bmp")
-image_music_play_bits = displayio.OnDiskBitmap("/image_music_play_bits.bmp")
-image_music_pause_bits = displayio.OnDiskBitmap("/image_music_pause_bits.bmp")
+brightnessImg = displayio.OnDiskBitmap("/brightnessImg.bmp")
+volumeImg = displayio.OnDiskBitmap("/volumeImg.bmp")
+playIconImg = displayio.OnDiskBitmap("/playIconImg.bmp")
+pauseIconImg = displayio.OnDiskBitmap("/pauseIconImg.bmp")
 
 
 Default = displayio.Group()
@@ -19,12 +19,12 @@ def drawDefault():
     rect_1 = Rect(117, 10, 7, 51, outline=0xFFFFFF)
     Default.append(rect_1)
     image_Volup_tile = displayio.TileGrid(
-        image_Volup_bits, pixel_shader=image_Volup_bits.pixel_shader, position=(3, 2)
+        volumeImg, pixel_shader=volumeImg.pixel_shader, position=(3, 2)
     )
     Default.append(image_Volup_tile)
     image_imgbitmap_png_tile = displayio.TileGrid(
-        image_imgbitmap_png_bits,
-        pixel_shader=image_imgbitmap_png_bits.pixel_shader,
+        brightnessImg,
+        pixel_shader=brightnessImg.pixel_shader,
         position=(117, 2),
     )
     Default.append(image_imgbitmap_png_tile)
@@ -44,98 +44,97 @@ def drawDefault():
     Default.append(rect_7)
 
 
+mediaPlay = displayio.Group()
 
-media__play_icon_ = displayio.Group()
 
-
-def drawmedia__play_icon_():
+def drawMediaPlay():
     rect_1 = Rect(4, 10, 7, 51, outline=0xFFFFFF)
-    media__play_icon_.append(rect_1)
+    mediaPlay.append(rect_1)
     rect_1 = Rect(117, 10, 7, 51, outline=0xFFFFFF)
-    media__play_icon_.append(rect_1)
+    mediaPlay.append(rect_1)
     image_Volup_tile = displayio.TileGrid(
-        image_Volup_bits, pixel_shader=image_Volup_bits.pixel_shader, position=(3, 2)
+        volumeImg, pixel_shader=volumeImg.pixel_shader, position=(3, 2)
     )
-    media__play_icon_.append(image_Volup_tile)
+    mediaPlay.append(image_Volup_tile)
     image_imgbitmap_png_tile = displayio.TileGrid(
-        image_imgbitmap_png_bits,
-        pixel_shader=image_imgbitmap_png_bits.pixel_shader,
+        brightnessImg,
+        pixel_shader=brightnessImg.pixel_shader,
         position=(117, 2),
     )
-    media__play_icon_.append(image_imgbitmap_png_tile)
+    mediaPlay.append(image_imgbitmap_png_tile)
     string_5 = label.Label(terminalio.FONT, text="88:88", color=0xFFFFFF)
     string_5.x = 42
     string_5.y = 11
-    media__play_icon_.append(string_5)
+    mediaPlay.append(string_5)
     string_6 = label.Label(terminalio.FONT, text="AM", color=0xFFFFFF)
     string_6.x = 74
     string_6.y = 11
-    media__play_icon_.append(string_6)
+    mediaPlay.append(string_6)
     rect_7 = Rect(4, 48, 7, 12, fill=0xFFFFFF, outline=0xFFFFFF)
-    media__play_icon_.append(rect_7)
+    mediaPlay.append(rect_7)
     rect_7 = Rect(117, 48, 7, 12, fill=0xFFFFFF, outline=0xFFFFFF)
-    media__play_icon_.append(rect_7)
+    mediaPlay.append(rect_7)
     string_17 = label.Label(terminalio.FONT, text="Current", color=0xFFFFFF)
     string_17.x = 42
     string_17.y = 31
-    media__play_icon_.append(string_17)
+    mediaPlay.append(string_17)
     rect_18 = Rect(21, 54, 86, 5, outline=0xFFFFFF)
-    media__play_icon_.append(rect_18)
+    mediaPlay.append(rect_18)
     rect_19 = Rect(21, 55, 21, 4, fill=0xFFFFFF, outline=0xFFFFFF)
-    media__play_icon_.append(rect_19)
+    mediaPlay.append(rect_19)
     circle_20 = Circle(41, 56, 4, fill=0xFFFFFF, outline=0xFFFFFF)
-    media__play_icon_.append(circle_20)
+    mediaPlay.append(circle_20)
     image_music_play_tile = displayio.TileGrid(
-        image_music_play_bits,
-        pixel_shader=image_music_play_bits.pixel_shader,
+        playIconImg,
+        pixel_shader=playIconImg.pixel_shader,
         position=(61, 42),
     )
-    media__play_icon_.append(image_music_play_tile)
+    mediaPlay.append(image_music_play_tile)
 
 
-media__pause_icon_ = displayio.Group()
+mediaPause = displayio.Group()
 
 
-def drawmedia__pause_icon_():
+def drawMediaPause():
     rect_1 = Rect(4, 10, 7, 51, outline=0xFFFFFF)
-    media__pause_icon_.append(rect_1)
+    mediaPause.append(rect_1)
     rect_1 = Rect(117, 10, 7, 51, outline=0xFFFFFF)
-    media__pause_icon_.append(rect_1)
+    mediaPause.append(rect_1)
     image_Volup_tile = displayio.TileGrid(
-        image_Volup_bits, pixel_shader=image_Volup_bits.pixel_shader, position=(3, 2)
+        volumeImg, pixel_shader=volumeImg.pixel_shader, position=(3, 2)
     )
-    media__pause_icon_.append(image_Volup_tile)
+    mediaPause.append(image_Volup_tile)
     image_imgbitmap_png_tile = displayio.TileGrid(
-        image_imgbitmap_png_bits,
-        pixel_shader=image_imgbitmap_png_bits.pixel_shader,
+        brightnessImg,
+        pixel_shader=brightnessImg.pixel_shader,
         position=(117, 2),
     )
-    media__pause_icon_.append(image_imgbitmap_png_tile)
+    mediaPause.append(image_imgbitmap_png_tile)
     string_5 = label.Label(terminalio.FONT, text="88:88", color=0xFFFFFF)
     string_5.x = 42
     string_5.y = 11
-    media__pause_icon_.append(string_5)
+    mediaPause.append(string_5)
     string_6 = label.Label(terminalio.FONT, text="AM", color=0xFFFFFF)
     string_6.x = 74
     string_6.y = 11
-    media__pause_icon_.append(string_6)
+    mediaPause.append(string_6)
     rect_7 = Rect(4, 48, 7, 12, fill=0xFFFFFF, outline=0xFFFFFF)
-    media__pause_icon_.append(rect_7)
+    mediaPause.append(rect_7)
     rect_7 = Rect(117, 48, 7, 12, fill=0xFFFFFF, outline=0xFFFFFF)
-    media__pause_icon_.append(rect_7)
+    mediaPause.append(rect_7)
     string_17 = label.Label(terminalio.FONT, text="Current", color=0xFFFFFF)
     string_17.x = 42
     string_17.y = 31
-    media__pause_icon_.append(string_17)
+    mediaPause.append(string_17)
     rect_18 = Rect(21, 54, 86, 5, outline=0xFFFFFF)
-    media__pause_icon_.append(rect_18)
+    mediaPause.append(rect_18)
     rect_19 = Rect(21, 55, 21, 4, fill=0xFFFFFF, outline=0xFFFFFF)
-    media__pause_icon_.append(rect_19)
+    mediaPause.append(rect_19)
     circle_20 = Circle(41, 56, 4, fill=0xFFFFFF, outline=0xFFFFFF)
-    media__pause_icon_.append(circle_20)
+    mediaPause.append(circle_20)
     image_music_play_tile = displayio.TileGrid(
-        image_music_pause_bits,
-        pixel_shader=image_music_pause_bits.pixel_shader,
+        pauseIconImg,
+        pixel_shader=pauseIconImg.pixel_shader,
         position=(61, 42),
     )
-    media__pause_icon_.append(image_music_play_tile)
+    mediaPause.append(image_music_play_tile)
