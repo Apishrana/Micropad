9beb2033650401d1190ffa0314104435343fa2fb
Apishrana <apishrana682@gmail.com>
2026-09-18 17:11:31 +0530

    added hepler functions to update display

--- src/display.py
+++ src/display.py
@@ -15,8 +15,12 @@ Default = displayio.Group()
 
 volumeBarOutline = Rect(4, 10, 7, 51, outline=0xFFFFFF)
 Default.append(volumeBarOutline)
+volumeBarFill = Rect(5, 11, 5, 49, fill=0xFFFFFF, outline=0xFFFFFF)
+Default.append(volumeBarFill)
 brightnessBarOutline = Rect(117, 10, 7, 51, outline=0xFFFFFF)
 Default.append(brightnessBarOutline)
+brightnessBarFill = Rect(118, 49, 5, 12, fill=0xFFFFFF, outline=0xFFFFFF)
+Default.append(brightnessBarFill)
 image_Volup_tile = displayio.TileGrid(
     volumeImg, pixel_shader=volumeImg.pixel_shader, position=(3, 2)
 )
@@ -37,101 +41,115 @@ timeSuffixString.x = 83
 timeSuffixString.y = 32
 timeSuffixString.scale = 2
 Default.append(timeSuffixString)
-volumeBarFill = Rect(4, 49, 7, 12, fill=0xFFFFFF, outline=0xFFFFFF)
-Default.append(volumeBarFill)
-brightnessBarFill = Rect(117, 49, 7, 12, fill=0xFFFFFF, outline=0xFFFFFF)
-Default.append(brightnessBarFill)
 
 
-mediaPlay = displayio.Group()
+def updateTimeDefault(time, suffix):
+    timeString.text = time
+    timeSuffixString.text = suffix
+
+
+def updateVolumeDefault(percent):
+    h = int(49 / 100 * percent)
+    volumeBarFill.height = h
+    volumeBarFill.y = 60 - h
+    if h == 0:
+        volumeBarFill.fill = 0x000000
+        volumeBarFill.outline = 0x000000
+    else:
+        volumeBarFill.fill = 0xFFFFFF
+        volumeBarFill.outline = 0xFFFFFF
+
+
+def updateBrightnessDefault(percent):
+    h = int(49 / 100 * percent)
+    brightnessBarFill.height = h
+    brightnessBarFill.y = 60 - h
+    if h == 0:
+        brightnessBarFill.fill = 0x000000
+        brightnessBarFill.outline = 0x000000
+    else:
+        brightnessBarFill.fill = 0xFFFFFF
+        brightnessBarFill.outline = 0xFFFFFF
+
+
+media = displayio.Group()
 
 
 volumeBarOutline = Rect(4, 10, 7, 51, outline=0xFFFFFF)
-mediaPlay.append(volumeBarOutline)
+media.append(volumeBarOutline)
+volumeBarFillMedia = Rect(4, 49, 7, 12, fill=0xFFFFFF, outline=0xFFFFFF)
+media.append(volumeBarFillMedia)
 brightnessBarOutline = Rect(117, 10, 7, 51, outline=0xFFFFFF)
-mediaPlay.append(brightnessBarOutline)
+media.append(brightnessBarOutline)
+brightnessBarFillMedia = Rect(117, 49, 7, 12, fill=0xFFFFFF, outline=0xFFFFFF)
+media.append(brightnessBarFillMedia)
 image_Volup_tile = displayio.TileGrid(
     volumeImg, pixel_shader=volumeImg.pixel_shader, position=(3, 2)
 )
-mediaPlay.append(image_Volup_tile)
+media.append(image_Volup_tile)
 image_imgbitmap_png_tile = displayio.TileGrid(
     brightnessImg,
     pixel_shader=brightnessImg.pixel_shader,
     position=(117, 2),
 )
-mediaPlay.append(image_imgbitmap_png_tile)
-timeString = label.Label(terminalio.FONT, text="88:88", color=0xFFFFFF)
-timeString.x = 42
-timeString.y = 11
-mediaPlay.append(timeString)
-timeSuffixString = label.Label(terminalio.FONT, text="AM", color=0xFFFFFF)
-timeSuffixString.x = 74
-timeSuffixString.y = 11
-mediaPlay.append(timeSuffixString)
-volumeBarFill = Rect(4, 49, 7, 12, fill=0xFFFFFF, outline=0xFFFFFF)
-mediaPlay.append(volumeBarFill)
-brightnessBarFill = Rect(117, 49, 7, 12, fill=0xFFFFFF, outline=0xFFFFFF)
-mediaPlay.append(brightnessBarFill)
+media.append(image_imgbitmap_png_tile)
+timeStringMedia = label.Label(terminalio.FONT, text="88:88", color=0xFFFFFF)
+timeStringMedia.x = 42
+timeStringMedia.y = 11
+media.append(timeStringMedia)
+timeSuffixStringMedia = label.Label(terminalio.FONT, text="AM", color=0xFFFFFF)
+timeSuffixStringMedia.x = 74
+timeSuffixStringMedia.y = 11
+media.append(timeSuffixStringMedia)
 string_17 = label.Label(terminalio.FONT, text="Current", color=0xFFFFFF)
 string_17.x = 42
 string_17.y = 31
-mediaPlay.append(string_17)
+media.append(string_17)
 mediaBarOutline = Rect(21, 54, 86, 5, outline=0xFFFFFF)
-mediaPlay.append(mediaBarOutline)
+media.append(mediaBarOutline)
 mediaBarFill = Rect(21, 55, 21, 4, fill=0xFFFFFF, outline=0xFFFFFF)
-mediaPlay.append(mediaBarFill)
+media.append(mediaBarFill)
 mediaCircle = Circle(41, 56, 4, fill=0xFFFFFF, outline=0xFFFFFF)
-mediaPlay.append(mediaCircle)
+media.append(mediaCircle)
 image_music_play_tile = displayio.TileGrid(
-    playIconImg,
+    pauseIconImg,
     pixel_shader=playIconImg.pixel_shader,
     position=(61, 42),
 )
-mediaPlay.append(image_music_play_tile)
+media.append(image_music_play_tile)
 
 
-mediaPause = displayio.Group()
+def setPlaying(playing):
+    if playing:
+        image_music_play_tile.bitmap = pauseIconImg
+    else:
+        image_music_play_tile.bitmap = playIconImg
 
 
-volumeBarOutline = Rect(4, 10, 7, 51, outline=0xFFFFFF)
-mediaPause.append(volumeBarOutline)
-brightnessBarOutline = Rect(117, 10, 7, 51, outline=0xFFFFFF)
-mediaPause.append(brightnessBarOutline)
-image_Volup_tile = displayio.TileGrid(
-    volumeImg, pixel_shader=volumeImg.pixel_shader, position=(3, 2)
-)
-mediaPause.append(image_Volup_tile)
-image_imgbitmap_png_tile = displayio.TileGrid(
-    brightnessImg,
-    pixel_shader=brightnessImg.pixel_shader,
-    position=(117, 2),
-)
-mediaPause.append(image_imgbitmap_png_tile)
-timeString = label.Label(terminalio.FONT, text="88:88", color=0xFFFFFF)
-timeString.x = 42
-timeString.y = 11
-mediaPause.append(timeString)
-timeSuffixString = label.Label(terminalio.FONT, text="AM", color=0xFFFFFF)
-timeSuffixString.x = 74
-timeSuffixString.y = 11
-mediaPause.append(timeSuffixString)
-volumeBarFill = Rect(4, 49, 7, 12, fill=0xFFFFFF, outline=0xFFFFFF)
-mediaPause.append(volumeBarFill)
-brightnessBarFill = Rect(117, 49, 7, 12, fill=0xFFFFFF, outline=0xFFFFFF)
-mediaPause.append(brightnessBarFill)
-string_17 = label.Label(terminalio.FONT, text="Current", color=0xFFFFFF)
-string_17.x = 42
-string_17.y = 31
-mediaPause.append(string_17)
-mediaBarOutline = Rect(21, 54, 86, 5, outline=0xFFFFFF)
-mediaPause.append(mediaBarOutline)
-mediaBarFill = Rect(21, 55, 21, 4, fill=0xFFFFFF, outline=0xFFFFFF)
-mediaPause.append(mediaBarFill)
-mediaCircle = Circle(41, 56, 4, fill=0xFFFFFF, outline=0xFFFFFF)
-mediaPause.append(mediaCircle)
-image_music_play_tile = displayio.TileGrid(
-    pauseIconImg,
-    pixel_shader=pauseIconImg.pixel_shader,
-    position=(61, 42),
-)
-mediaPause.append(image_music_play_tile)
+def updateTimeMedia(time, suffix):
+    timeStringMedia.text = time
+    timeSuffixStringMedia.text = suffix
+
+
+def updateVolumeMedia(percent):
+    h = int(49 / 100 * percent)
+    volumeBarFillMedia.height = h
+    volumeBarFillMedia.y = 60 - h
+    if h == 0:
+        volumeBarFillMedia.fill = 0x000000
+        volumeBarFillMedia.outline = 0x000000
+    else:
+        volumeBarFillMedia.fill = 0xFFFFFF
+        volumeBarFillMedia.outline = 0xFFFFFF
+
+
+def updateBrightnessMedia(percent):
+    h = int(49 / 100 * percent)
+    brightnessBarFillMedia.height = h
+    brightnessBarFillMedia.y = 60 - h
+    if h == 0:
+        brightnessBarFillMedia.fill = 0x000000
+        brightnessBarFillMedia.outline = 0x000000
+    else:
+        brightnessBarFillMedia.fill = 0xFFFFFF
+        brightnessBarFillMedia.outline = 0xFFFFFF
